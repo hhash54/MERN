d597abb2da73c605697aa5169b21de657946a91e
hhash54 <hhashem2002@gmail.com>
2023-06-26 17:00:03 -0400

    changes full

--- api/index.js
+++ api/index.js
@@ -1,19 +1,18 @@
 const express = require('express');
-const cookieParser = require('cookie-parser');
-const app = express();
 const cors = require('cors');
-const mongoose = require('mongoose');
-const User = require('./models/User.js');
+const mongoose = require("mongoose");
 const bcrypt = require('bcryptjs');
-const Place=require('./models/Place.js');
+const jwt = require('jsonwebtoken');
+const User = require('./models/User.js');
+const Place = require('./models/Place.js');
 const Booking = require('./models/Booking.js');
+const cookieParser = require('cookie-parser');
+const imageDownloader = require('image-downloader');
+const multer = require('multer');
+const fs = require('fs');
 
 require('dotenv').config();
-const jwt= require('jsonwebtoken');
-const imageDownloader= require('image-downloader');
-const multer=require('multer');
-const fs=require('fs');
-
+const app = express();
 
 const bcryptSalt = bcrypt.genSaltSync(10);
 const jwtSecret='dsf6ds7f';
@@ -25,100 +24,101 @@ app.use(cors({
   credentials: true,
   origin: 'http://localhost:5173',
 }));
-
 mongoose.connect(process.env.MONGO_URL);
 
-const db = mongoose.connection;
+function getUserDataFromReq(req) {
+  return new Promise((resolve, reject) => {
+    jwt.verify(req.cookies.token, jwtSecret, {}, async (err, userData) => {
+      if (err) throw err;
+      resolve(userData);
+    });
+  });
+}
 
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => {
-  console.log('Connected to MongoDB');
-});
-
-app.get('/test', (req, res) => {
+app.get('/test', (req,res) => {
   res.json('test ok');
 });
 
-app.post('/register', async (req, res) => {
-  const { name, email, password } = req.body;
+app.post('/register', async (req,res) => {
+  const {name,email,password} = req.body;
 
   try {
-    const userDoc = await User.create({ name, 
-        email, 
-        password: bcrypt.hashSync(password, bcryptSalt) });
+    const userDoc = await User.create({
+      name,
+      email,
+      password:bcrypt.hashSync(password, bcryptSalt),
+    });
     res.json(userDoc);
   } catch (e) {
     res.status(422).json(e);
   }
+
 });
 
 app.post('/login', async (req,res) => {
-    mongoose.connect(process.env.MONGO_URL);
-    const {email,password} = req.body;
-    const userDoc = await User.findOne({email});
-    if (userDoc) {
-      const passOk = bcrypt.compareSync(password, userDoc.password);
-      if (passOk) {
-        jwt.sign({
-          email:userDoc.email,
-          id:userDoc._id
-        }, jwtSecret, {}, (err,token) => {
-          if (err) throw err;
-          res.cookie('token', token).json(userDoc);
-        });
-      } else {
-        res.status(422).json('pass not ok');
-      }
-    } else {
-      res.json('not found');
-    }
-  });
-  
-  app.get('/profile', (req,res) => {
-    mongoose.connect(process.env.MONGO_URL);
-    const {token} = req.cookies;
-    if (token) {
-      jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+  const {email,password} = req.body;
+  const userDoc = await User.findOne({email});
+  if (userDoc) {
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+    if (passOk) {
+      jwt.sign({
+        email:userDoc.email,
+        id:userDoc._id
+      }, jwtSecret, {}, (err,token) => {
         if (err) throw err;
-        const {name,email,_id} = await User.findById(userData.id);
-        res.json({name,email,_id});
+        res.cookie('token', token).json(userDoc);
       });
     } else {
-      res.json(null);
+      res.status(422).json('pass not ok');
     }
-  });
-  app.post('/logout',(req,res) => {
-    
-    res.cookie('token','').json(true);
-  });
-
-app.post('/upload-by-link', async (req, res) => {
-    const { link } = req.body;
-    const newName = 'photo'+Date.now() + '.jpg';
-    await imageDownloader.image({
-        url: link,
-        dest: __dirname + '/uploads/' + newName,
-    });
-    res.json(newName);
+  } else {
+    res.json('not found');
+  }
 });
 
-const photosMiddleware=multer({dest:'uploads'})
-app.post('/upload', photosMiddleware.array('photos',100),(req, res) => {
-    const uploadedFiles=[];
-    for (let i=0; i < req.files.length; i++){
-        const {path,originalname}=req.files[i];
-        const parts=originalname.split('.');
-        const ext=parts[parts.length - 1];
-        const newPath=path+'.'+ext;
-        fs.renameSync(path,newPath);
-        uploadedFiles.push(newPath.replace('uploads/',''));
-    }
-    res.json(uploadedFiles);
-    
-})
+app.get('/profile', (req,res) => {
+  const {token} = req.cookies;
+  if (token) {
+    jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+      if (err) throw err;
+      const {name,email,_id} = await User.findById(userData.id);
+      res.json({name,email,_id});
+    });
+  } else {
+    res.json(null);
+  }
+});
+
+app.post('/logout', (req,res) => {
+  res.cookie('token', '').json(true);
+});
+
+
+app.post('/upload-by-link', async (req,res) => {
+  const {link} = req.body;
+  const newName = 'photo' + Date.now() + '.jpg';
+  await imageDownloader.image({
+    url: link,
+    dest: __dirname + '/uploads/' +newName,
+  });
+  res.json(newName);
+});
+
+const photosMiddleware = multer({dest:'uploads/'});
+app.post('/upload', photosMiddleware.array('photos', 100), (req,res) => {
+  const uploadedFiles = [];
+  for (let i = 0; i < req.files.length; i++) {
+    const {path,originalname} = req.files[i];
+    const parts = originalname.split('.');
+    const ext = parts[parts.length - 1];
+    const newPath = path + '.' + ext;
+    fs.renameSync(path, newPath);
+    uploadedFiles.push(newPath.replace('uploads/',''));
+  }
+  res.json(uploadedFiles);
+});
 
 app.post('/places', (req,res) => {
-  mongoose.connect(process.env.MONGO_URL);
   const {token} = req.cookies;
   const {
     title,address,addedPhotos,description,price,
@@ -134,19 +134,20 @@ app.post('/places', (req,res) => {
     res.json(placeDoc);
   });
 });
-app.get('/user-places', (req, res)=>{
+
+app.get('/user-places', (req,res) => {
   const {token} = req.cookies;
-
   jwt.verify(token, jwtSecret, {}, async (err, userData) => {
-    const {id}= userData;
-    res.json(await Place.find({owner:id}))
-  })
-})
+    const {id} = userData;
+    res.json( await Place.find({owner:id}) );
+  });
+});
 
-app.get('/places/:id', async (req, res)=>{
-  const {id}=req.params;
+app.get('/places/:id', async (req,res) => {
+  const {id} = req.params;
   res.json(await Place.findById(id));
-})
+});
+
 app.put('/places', async (req,res) => {
   const {token} = req.cookies;
   const {
@@ -167,6 +168,10 @@ app.put('/places', async (req,res) => {
   });
 });
 
+app.get('/places', async (req,res) => {
+  res.json( await Place.find() );
+});
+
 app.post('/bookings', async (req, res) => {
   const userData = await getUserDataFromReq(req);
   const {
@@ -181,17 +186,12 @@ app.post('/bookings', async (req, res) => {
     throw err;
   });
 });
+
+
+
 app.get('/bookings', async (req,res) => {
   const userData = await getUserDataFromReq(req);
   res.json( await Booking.find({user:userData.id}).populate('place') );
 });
 
-  
-app.listen(4000, () => {
-  console.log('Server listening on port 4000');
-});
-
-app.get('/places',async(req, res) => {
-  res.json(await Place.find());
-})
-
+app.listen(4000);
--- client/src/pages/PlacesFormPage.jsx
+++ client/src/pages/PlacesFormPage.jsx
@@ -1,89 +1,89 @@
-import PhotosUploader from "../PhotosUploader";
-import axios from "axios";
+import PhotosUploader from "../PhotosUploader.jsx";
 import Perks from "../Perks.jsx";
-import { useState, useEffect } from "react";
+import {useEffect, useState} from "react";
+import axios from "axios";
 import AccountNav from "../AccountNav";
-import { Navigate, useParams } from "react-router-dom";
-export default function PlacesFormpage(){
-    const {id}=useParams();
-    const [title, setTitle] = useState('');
-    const [address, setAddress] = useState('');
-    const [addedPhotos, setAddedPhotos] = useState([]);
-    const [description, setDescription] = useState('');
-    const [perks, setPerks] = useState([]);
-    const [extraInfo, setExtraInfo] = useState('');
-    const [checkIn, setCheckIn] = useState('');
-    const [checkOut, setCheckOut] = useState('');
-    const [maxGuests, setMaxGuests] = useState (1);
-    const [price,setPrice] = useState(100);
+import {Navigate, useParams} from "react-router-dom";
 
-    const[redirect, setRedirect]=useState(false);
-useEffect(() => {
-    if(!id){
-        return;
+export default function PlacesFormPage() {
+  const {id} = useParams();
+  const [title,setTitle] = useState('');
+  const [address,setAddress] = useState('');
+  const [addedPhotos,setAddedPhotos] = useState([]);
+  const [description,setDescription] = useState('');
+  const [perks,setPerks] = useState([]);
+  const [extraInfo,setExtraInfo] = useState('');
+  const [checkIn,setCheckIn] = useState('');
+  const [checkOut,setCheckOut] = useState('');
+  const [maxGuests,setMaxGuests] = useState(1);
+  const [price,setPrice] = useState(100);
+  const [redirect,setRedirect] = useState(false);
+  useEffect(() => {
+    if (!id) {
+      return;
     }
     axios.get('/places/'+id).then(response => {
-        const {data}=response;
-        setTitle(data.title);
-        setAddress(data.address);
-        setAddedPhotos(data.photos);
-        setDescription(data.description);
-        setPerks(data.perks);
-        setExtraInfo(data.extraInfo);
-        setCheckIn(data.checkIn);
-        setCheckOut(data.checkOut);
-        setMaxGuests(data.maxGuests);
-        setPrice(data.price);
-
+       const {data} = response;
+       setTitle(data.title);
+       setAddress(data.address);
+       setAddedPhotos(data.photos);
+       setDescription(data.description);
+       setPerks(data.perks);
+       setExtraInfo(data.extraInfo);
+       setCheckIn(data.checkIn);
+       setCheckOut(data.checkOut);
+       setMaxGuests(data.maxGuests);
+       setPrice(data.price);
     });
-},[id])
-   
+  }, [id]);
+  function inputHeader(text) {
+    return (
+      <h2 className="text-2xl mt-4">{text}</h2>
+    );
+  }
+  function inputDescription(text) {
+    return (
+      <p className="text-gray-500 text-sm">{text}</p>
+    );
+  }
+  function preInput(header,description) {
+    return (
+      <>
+        {inputHeader(header)}
+        {inputDescription(description)}
+      </>
+    );
+  }
 
-    function inputHeader(text) {
-        return <h2 className="text-2xl mt-4">{text}</h2>
+  async function savePlace(ev) {
+    ev.preventDefault();
+    const placeData = {
+      title, address, addedPhotos,
+      description, perks, extraInfo,
+      checkIn, checkOut, maxGuests, price,
+    };
+    if (id) {
+      // update
+      await axios.put('/places', {
+        id, ...placeData
+      });
+      setRedirect(true);
+    } else {
+      // new place
+      await axios.post('/places', placeData);
+      setRedirect(true);
     }
-    
-    
-    function inputDescription(text) {
-        return (<p className="text-gray-500 text-sm">{text}</p>)
-    }
-    
-    function preInput(header, description) {
-        return (
-            <>
-                {inputHeader(header)}
-                {inputDescription(description)}
-            </>
-        )
-    }
-    async function savePlace(ev) {
-        ev.preventDefault();
-        const placeData = {
-          title, address, addedPhotos,
-          description, perks, extraInfo,
-          checkIn, checkOut, maxGuests,price,
-        };
-        if (id) {
-          // update
-          await axios.put('/places', {
-            id, ...placeData
-          });
-          setRedirect(true);
-        } else {
-          // new place
-          await axios.post('/places', placeData);
-          setRedirect(true);
-        }
-        
-    
-      }
-      if (redirect) {
-        return <Navigate to={'/account/places'} />
-      }
-    return(
-        <div>
-            <AccountNav/>
-            <form onSubmit={savePlace}>
+
+  }
+
+  if (redirect) {
+    return <Navigate to={'/account/places'} />
+  }
+
+  return (
+    <div>
+      <AccountNav />
+      <form onSubmit={savePlace}>
         {preInput('Title', 'Title for your place. should be short and catchy as in advertisement')}
         <input type="text" value={title} onChange={ev => setTitle(ev.target.value)} placeholder="title, for example: My lovely apt"/>
         {preInput('Address', 'Address to this place')}
@@ -127,6 +127,6 @@ useEffect(() => {
         </div>
         <button className="primary my-4">Save</button>
       </form>
-       </div> 
-    );
+    </div>
+  );
 }
